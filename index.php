8550e9b200f9095e5bf3b2787dd9cf5f3114da21
yohei Yuda <yuyu.history.journey@gmail.com>
2024-06-10 21:20:31 +0900

    コード修正

--- index.php
+++ index.php
@@ -1,51 +1,45 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-  <title>FizzBuzz問題</title>
+    <title>FizzBuzz問題</title>
 </head>
-
 <body>
-  <form method="post" action="">
-    <label for="fizzNum">Fizzの値:</label>
-    <input type="text" id="fizzNum" name="fizzNum" placeholder="ここに整数値を入力">
-    <br><br>
-    <label for="buzzNum">Buzzの値:</label>
-    <input type="text" id="buzzNum" name="buzzNum" placeholder="ここに整数値を入力">
-    <br><br>
-    <input type="submit" name="submit" value="実行">
-  </form>
-  <h2>出力</h2>
+    <form method="post" action="">
+        <label for="fizzNum">Fizzの値:</label>
+        <input type="text" id="fizzNum" name="fizzNum" placeholder="ここに整数値を入力">
+        <br><br>
+        <label for="buzzNum">Buzzの値:</label>
+        <input type="text" id="buzzNum" name="buzzNum" placeholder="ここに整数値を入力">
+        <br><br>
+        <input type="submit" name="submit" value="実行">
+    </form>
+    <h2>出力</h2>
 
-  <?php
-  $fizzNum = $_POST['fizzNum'];
-  $buzzNum = $_POST['buzzNum'];
-  if (isset($_POST['submit'])) {
+    <?php
+        $fizzNum = $_POST['fizzNum'];
+        $buzzNum = $_POST['buzzNum'];
+    if (isset($_POST['submit'])) {
 
 
-    if (empty($fizzNum) || empty($buzzNum)) {
-      echo "<p>整数値を入力してください</p>";
-    } elseif (!ctype_digit($fizzNum) || !ctype_digit($buzzNum)) {
-      echo "<p>整数値を入力してください</p>";
-    } else {
-      $fizzNum = (int)$fizzNum;
-      $buzzNum = (int)$buzzNum;
-
-      for ($i = 1; $i < 100; $i++) {
-        $output = "";
-        if ($i % $fizzNum == 0) {
-          $output .= "fizz";
+        if (empty($fizzNum) || empty($buzzNum)) {
+            echo "<p>整数値を入力してください</p>";
+        } elseif (!ctype_digit($fizzNum) || !ctype_digit($buzzNum)) {
+            echo "<p>整数値を入力してください</p>";
+        } else {
+            $fizzNum = (int)$fizzNum;
+            $buzzNum = (int)$buzzNum;
+            
+            for ($i = 1; $i < 100; $i++) {
+                if ($i % $fizzNum == 0 && $i % $buzzNum == 0) {
+                    echo "<p>$i: fizzBuzz</p>";
+                } elseif ($i % $fizzNum == 0) {
+                    echo "<p>$i: fizz</p>";
+                } elseif ($i % $buzzNum == 0) {
+                    echo "<p>$i: buzz</p>";
+                }
+            }
         }
-        if ($i % $buzzNum == 0) {
-          $output .= "buzz";
-        }
-        if ($output != "") {
-          echo "<p>$i: $output</p>";
-        }
-      }
     }
-  }
-  ?>
+    ?>
 </body>
-
 </html>
