cb91c37d073b9a5b2d884a1b3893ca4ac4b74c68
yoheihe <yuyu.history.journey@gmail.com>
2024-06-15 07:27:25 +0900

    PHP課題②に取り組みました (#1)
    * 課題作成済
    
    * コード修正
    
    * コード修正

--- index.php
+++ index.php
@@ -1,1 +1,45 @@
-test
+<!DOCTYPE html>
+<html>
+<head>
+    <title>FizzBuzz問題</title>
+</head>
+<body>
+    <form method="post" action="">
+        <label for="fizzNum">Fizzの値:</label>
+        <input type="text" id="fizzNum" name="fizzNum" placeholder="ここに整数値を入力">
+        <br><br>
+        <label for="buzzNum">Buzzの値:</label>
+        <input type="text" id="buzzNum" name="buzzNum" placeholder="ここに整数値を入力">
+        <br><br>
+        <input type="submit" name="submit" value="実行">
+    </form>
+    <h2>出力</h2>
+
+    <?php
+        $fizzNum = $_POST['fizzNum'];
+        $buzzNum = $_POST['buzzNum'];
+    if (isset($_POST['submit'])) {
+
+
+        if (empty($fizzNum) || empty($buzzNum)) {
+            echo "<p>整数値を入力してください</p>";
+        } elseif (!ctype_digit($fizzNum) || !ctype_digit($buzzNum)) {
+            echo "<p>整数値を入力してください</p>";
+        } else {
+            $fizzNum = (int)$fizzNum;
+            $buzzNum = (int)$buzzNum;
+            
+            for ($i = 1; $i < 100; $i++) {
+                if ($i % $fizzNum === 0 && $i % $buzzNum === 0) {
+                    echo "<p>$i: fizzBuzz</p>";
+                } elseif ($i % $fizzNum === 0) {
+                    echo "<p>$i: fizz</p>";
+                } elseif ($i % $buzzNum === 0) {
+                    echo "<p>$i: buzz</p>";
+                }
+            }
+        }
+    }
+    ?>
+</body>
+</html>
